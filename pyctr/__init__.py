ad0d99a398f4e89c7835b34e0e8e7a6e970cafbd
Ian Burgwin <ian@ianburgwin.net>
2021-01-22 05:52:57 -0800

    update copyright year

--- pyctr/__init__.py
+++ pyctr/__init__.py
@@ -1,13 +1,13 @@
 # This file is a part of pyctr.
 #
-# Copyright (c) 2017-2020 Ian Burgwin
+# Copyright (c) 2017-2021 Ian Burgwin
 # This file is licensed under The MIT License (MIT).
 # You can find the full license text in LICENSE in the root of this project.
 
 from collections import namedtuple
 
 __author__ = 'ihaveamac'
-__copyright__ = 'Copyright (c) 2017-2020 Ian Burgwin'
+__copyright__ = 'Copyright (c) 2017-2021 Ian Burgwin'
 __license__ = 'MIT'
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
