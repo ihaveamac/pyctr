914c102ae11486bc4ec185b2738446eda0b87501
Ian Burgwin <ian@ianburgwin.net>
2022-01-26 19:02:32 -0800

    update copyright date to 2022

--- pyctr/crypto/__init__.py
+++ pyctr/crypto/__init__.py
@@ -1,6 +1,6 @@
 # This file is a part of pyctr.
 #
-# Copyright (c) 2017-2021 Ian Burgwin
+# Copyright (c) 2017-2022 Ian Burgwin
 # This file is licensed under The MIT License (MIT).
 # You can find the full license text in LICENSE in the root of this project.
 
