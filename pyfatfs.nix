7469e872163c4fa7ca8c05af3192a483292ab880
ihaveahax <ian@ianburgwin.net>
2024-09-20 23:13:24 -0500

    update pyfatfs.nix to 1.1.0

--- pyfatfs.nix
+++ pyfatfs.nix
@@ -1,22 +1,28 @@
-{ lib, buildPythonPackage, pythonOlder, fetchPypi, fs, pip }:
+{ lib, buildPythonPackage, pythonOlder, fetchPypi, fs, pip, setuptools, setuptools-scm }:
 
 buildPythonPackage rec {
   pname = "pyfatfs";
-  version = "1.0.5";
-  format = "setuptools";
+  version = "1.1.0";
+  pyproject = true;
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-5J6gYhGf32GYx7u8/ghYnYkZ40rCH19gTQ7YtcREly0=";
+    hash = "sha256-lyXM0KTaHAnCc1irvxDwjAQ6yEIQr1doA+CH9RorMOA=";
   };
 
   doCheck = false;
 
+  patches = [ ./pyfatfs-fix-deps.patch ];
+
+  buildInputs = [ setuptools setuptools-scm ];
+
   propagatedBuildInputs = [
     fs
-    pip
+    setuptools
+    setuptools-scm
+    #pip
   ];
 
   pythonImportsCheck = [
